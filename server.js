25f981a0f4ee941244b7b43bad437fb31817ae52
SewerDog <nnthxa729@gmail.com>
2026-02-27 01:20:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,19 @@
-const express = require("express");
-const axios = require("axios");
-const app = express();
+app.get("/spotify/status/:userid", async (req, res) => {
+  const userId = req.params.userid;
+  const token = getTokenForUser(userId);
 
-let access_token = null;
+  const response = await axios.get("https://api.spotify.com/v1/me/player", {
+    headers: { Authorization: `Bearer ${token}` }
+  });
 
-app.get("/login", (req, res) => {
-  const scope = "user-read-currently-playing user-read-playback-state";
+  const body = response.data;
 
-  const authUrl =
-    "https://accounts.spotify.com/authorize?" +
-    new URLSearchParams({
-      response_type: "code",
-      client_id: process.env.CLIENT_ID,
-      scope: scope,
-      redirect_uri: process.env.REDIRECT_URI,
-    });
-
-  res.redirect(authUrl);
+  res.json({
+    track: body.item?.name || null,
+    artist: (body.item?.artists || []).map(a => a.name).join(', ') || null,
+    is_playing: body.is_playing || false,
+    album: body?.item?.album?.name,
+    duration_ms: body?.item?.duration_ms,
+    progress_ms: body?.progress_ms
+  });
 });
-
-app.get("/callback", async (req, res) => {
-  const code = req.query.code;
-
-  const response = await axios.post(
-    "https://accounts.spotify.com/api/token",
-    new URLSearchParams({
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: process.env.REDIRECT_URI,
-    }),
-    {
-      headers: {
-        Authorization:
-          "Basic " +
-          Buffer.from(
-            process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
-          ).toString("base64"),
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }
-  );
-
-  access_token = response.data.access_token;
-
-  res.send("Login successful!");
-});
-
-app.get("/current", async (req, res) => {
-  if (!access_token) {
-    return res.json({ error: "Not logged in" });
-  }
-
-  const response = await axios.get(
-    "https://api.spotify.com/v1/me/player/currently-playing",
-    {
-      headers: {
-        Authorization: "Bearer " + access_token,
-      },
-    }
-  );
-
-  res.json(response.data);
-});
-
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Server running")
-);
