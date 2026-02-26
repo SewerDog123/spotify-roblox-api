c05cd4d2d83eec983e48fe62032b017fadf8c965
SewerDog <nnthxa729@gmail.com>
2026-02-27 01:07:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,33 @@
-require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 let access_token = null;
-let refresh_token = null;
-
-app.get("/", (req, res) => {
-  res.send("Spotify Roblox API Running");
-});
 
 app.get("/login", (req, res) => {
-  const scope = "user-read-playback-state";
-  res.redirect(
+  const scope = "user-read-currently-playing user-read-playback-state";
+
+  const authUrl =
     "https://accounts.spotify.com/authorize?" +
-      new URLSearchParams({
-        response_type: "code",
-        client_id: process.env.CLIENT_ID,
-        scope: scope,
-        redirect_uri: process.env.REDIRECT_URL,
-      })
-  );
+    new URLSearchParams({
+      response_type: "code",
+      client_id: process.env.CLIENT_ID,
+      scope: scope,
+      redirect_uri: process.env.REDIRECT_URI,
+    });
+
+  res.redirect(authUrl);
 });
 
 app.get("/callback", async (req, res) => {
-  try {
-    const code = req.query.code;
+  const code = req.query.code;
 
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      new URLSearchParams({
-        grant_type: "authorization_code",
-        code: code,
-        redirect_uri: process.env.REDIRECT_URL,
-      }),
-      {
-        headers: {
-          Authorization:
-            "Basic " +
-            Buffer.from(
-              process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
-            ).toString("base64"),
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-
-    access_token = response.data.access_token;
-    refresh_token = response.data.refresh_token;
-
-    res.send("Login successful!");
-  } catch (err) {
-    console.error(err.response?.data || err.message);
-    res.send("Login failed");
-  }
-});
-
-async function refreshAccessToken() {
   const response = await axios.post(
     "https://accounts.spotify.com/api/token",
     new URLSearchParams({
-      grant_type: "refresh_token",
-      refresh_token: refresh_token,
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: process.env.REDIRECT_URI,
     }),
     {
       headers: {
@@ -78,41 +42,27 @@ async function refreshAccessToken() {
   );
 
   access_token = response.data.access_token;
-}
+
+  res.send("Login successful!");
+});
 
 app.get("/current", async (req, res) => {
   if (!access_token) {
     return res.json({ error: "Not logged in" });
   }
 
-  try {
-    const response = await axios.get(
-      "https://api.spotify.com/v1/me/player",
-      {
-        headers: {
-          Authorization: "Bearer " + access_token,
-        },
-      }
-    );
-
-    const item = response.data.item;
-
-    if (!item) return res.json({ playing: false });
-
-    res.json({
-      track: item.name,
-      artist: item.artists.map(a => a.name).join(", "),
-      playing: response.data.is_playing,
-    });
-
-  } catch (err) {
-    if (err.response?.status === 401) {
-      await refreshAccessToken();
-      return res.redirect("/current");
+  const response = await axios.get(
+    "https://api.spotify.com/v1/me/player/currently-playing",
+    {
+      headers: {
+        Authorization: "Bearer " + access_token,
+      },
     }
+  );
 
-    res.json({ error: "Spotify API error" });
-  }
+  res.json(response.data);
 });
 
-app.listen(PORT, () => console.log("Server running"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server running")
+);
